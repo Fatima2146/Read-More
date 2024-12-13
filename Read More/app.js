913de18f0444f065b60335110594d25161af0794
Ayesha Ashraf <fatimaashraf1001@gmail.com>
2024-12-13 17:14:52 +0500

    Add files via upload

---
+++ Read More/app.js
@@ -0,0 +1,49 @@
+let ReadMore = document.getElementById("ReadMore");
+let extraContent = document.getElementById("extraContent");
+ReadMore.onclick = () => {
+    if (extraContent.style.display === "none") {
+      extraContent.style.display = "inline"; 
+      ReadMore.textContent = "Read Less"; 
+    } else {
+      extraContent.style.display = "none"; 
+      ReadMore.textContent = "Read More"; 
+    }
+  };
+
+let ReadMore2 = document.getElementById("ReadMore2");
+    let extraContent2 = document.getElementById("extraContent2");
+
+    ReadMore2.onclick = () => {
+      if (extraContent2.style.display === "none") {
+        extraContent2.style.display = "inline";
+        ReadMore2.textContent = "Read Less";
+      } else {
+        extraContent2.style.display = "none";
+        ReadMore2.textContent = "Read More"; 
+      }
+    };
+
+    let ReadMore3 = document.getElementById("ReadMore3");
+    let extraContent3 = document.getElementById("extraContent3");
+
+    ReadMore3.onclick = () => {
+      if (extraContent3.style.display === "none") {
+        extraContent3.style.display = "inline";
+        ReadMore3.textContent = "Read Less";
+      } else {
+        extraContent3.style.display = "none";
+        ReadMore3.textContent = "Read More"; 
+      }
+    };
+
+    let ReadMore4 = document.getElementById("ReadMore4");
+let extraContent4 = document.getElementById("extraContent4");
+ReadMore4.onclick = () => {
+    if (extraContent4.style.display === "none") {
+      extraContent4.style.display = "inline"; 
+      ReadMore4.textContent = "Read Less"; 
+    } else {
+      extraContent4.style.display = "none"; 
+      ReadMore4.textContent = "Read More"; 
+    }
+  };
